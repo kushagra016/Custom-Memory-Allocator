3b3d4efc5b85c61e2deb07bc691a2ba3a3e68a64
arsh-1192 <kingarsh624@gmail.com>
2026-09-23 11:47:16 +0530

    Added Terminal Visualizer

--- main/main.cpp
+++ main/main.cpp
@@ -77,6 +77,7 @@ public:
         return nullptr;
     }
 
+    //Deallocate Method
      void deallocate(void* ptr){
         if (ptr == nullptr) return;
 
@@ -103,6 +104,41 @@ public:
                 current->next->prev = current->prev;
             }
         }
+    }
+
+    //MemoryMap Method
+    void printMemoryMap(){
+        BlockHeader* current = head;
+        std:: cout << "\n=== Current Memory Map ===\n";
+
+        int blockCount = 0;
+        size_t totalFree = 0;
+        size_t totalUsed = 0;
+
+        while (current != nullptr){
+            std::cout << "[";
+            if (current->isFree){
+                std::cout << "FREE";
+                totalFree += current->size;
+            }
+            else {
+                std::cout << "USED";
+                totalUsed += current->size;
+            }
+            std::cout << ": " << current->size << "B] ";
+
+            if (current->next != nullptr){
+                std::cout << "->";
+            }
+
+            blockCount++;
+            current = current->next;
+        }
+
+        std::cout << "\nBlocks: " << blockCount
+                  << " | Used Data: " << totalUsed << "B"
+                  << " | Free Space: " << totalFree << "B\n";
+        std::cout << "===========================\n";
     }
 
 };
